78bd1c4f89754351b9df12e14a58a46d354b9926
meitham <m@meitham.com>
2012-12-29 22:49:30 +0000

    initial setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import os
+import sys
+
+
+version = __import__('pygnutools').VERSION
+
+scripts = ['bin/pyfind.py']
+
+if sys.platform == "win":
+    scripts.append('bin/pyfind.bat')
+else:
+    scripts.append('bin/pyfind')
+
+setup(name='pygnutools',
+    version=version,
+    description='Pure python implementation of GNU tools',
+    author='Meitham Jamaa',
+    author_email='meitham@meitham.com',
+    url='http://meitham.com/pygnutools/',
+    packages=['pygnutools'],
+    scripts = ,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Shell Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+ )
